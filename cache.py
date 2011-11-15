08d6f83fd958f8c461a442a0839ed052c8eb5066
Noriyuki Hosaka <nori@akagi.(none)>
2011-11-15 22:19:55 +0900

    kind http proxy for junkie. It is to keep things simple. 	new file:   cache.py 	new file:   test_cache.py

---
+++ cache.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding=utf8 -*-
+
+from twisted.web import client, xmlrpc, server
+from twisted.internet import reactor
+import sys
+
+storage = {}
+
+
+def printError(failure):
+  print >> sys.stderr, "Error", failure.getErrorMessage()
+
+class Cache(xmlrpc.XMLRPC):
+  def xmlrpc_get(self, url):
+    '''
+      return data pointed by url, None on no data
+    '''
+    return storage.get(url, None)
+  
+  def xmlrpc_fetch(self, url):
+    '''
+      request url and store the data from url in cache
+    '''
+    def storePage(data):
+      print >> sys.stderr, 'storing:', url
+      storage[url] = data
+
+    client.getPage(url)\
+      .addCallback(storePage)\
+      .addErrback(printError)
+  
+    return url #Echo.
+
+  def xmlrpc_pop(self, url):
+    '''
+      remove data pointed by the url from cache 
+    '''
+    return storage.pop(url)
+
+  def xmlrpc_clear(self):
+    storage.clear()
+    return None
+
+c = Cache(allowNone=True)
+
+reactor.listenTCP(9000, server.Site(c))
+reactor.run()
+
